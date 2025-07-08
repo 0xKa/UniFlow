c8dc5129a60b77fe9e081a327134e416cd69ccf5
Reda Hilal <reda0x04@gmail.com>
2025-07-08 18:33:57 +0400

    init api project

---
+++ UniFlow.API/Controllers/PeopleController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UniFlow.API.Models;
+
+namespace UniFlow.API.Controllers;
+
+
+[Route("api/People")]
+[ApiController]
+public class PeopleController : ControllerBase
+{
+    static List<Person> People = new List<Person>
+    {
+        new Person {ID = 1, FirstName = "John", LastName = "Doe", DateOfBirth = new DateTime(1990, 1, 1), Address = "123 Main St", Phone = "3902139210", Gender = 'M', ImagePath = null},
+        new Person {ID = 2, FirstName = "Jane", LastName = "Smith", DateOfBirth = new DateTime(1995, 5, 15), Address = "456 Elm St", Phone = "390213921", Gender = 'F', ImagePath = null},
+        new Person {ID = 3, FirstName = "Alice", LastName = "Johnson", DateOfBirth = new DateTime(1988, 3, 20), Address = "789 Oak St", Phone = "390213", Gender = 'F', ImagePath = null},
+    };
+
+    [HttpGet("All", Name = "GetAllPeople")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<IEnumerable<Person>> GetAllPeople()
+    {
+        List<Person> people = People;
+
+        if (people == null || people.Count == 0)
+            return NotFound("No People Found");
+
+        return Ok(people);
+
+    }
+
+
+
+}
---
+++ UniFlow.API/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace UniFlow.API.Models;
+
+public class Person
+{
+    public int ID { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+    public char Gender { get; set; }
+    public string Phone { get; set; } = string.Empty;
+    public string? Address { get; set; }
+    public string NationalID { get; set; } = string.Empty;
+    public string? ImagePath { get; set; }
+
+
+}
