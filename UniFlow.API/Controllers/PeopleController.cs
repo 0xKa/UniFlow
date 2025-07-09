95968f3ed89d55af77a71ea0686336c274ad3bdc
Reda Hilal <reda0x04@gmail.com>
2025-07-09 19:41:21 +0400

    call service

--- UniFlow.API/Controllers/PeopleController.cs
+++ UniFlow.API/Controllers/PeopleController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using UniFlow.API.Models;
+
+using BusinessLogic.Interfaces;
+using UniFlow.DTOs;
 
 namespace UniFlow.API.Controllers;
 
@@ -9,25 +11,40 @@ namespace UniFlow.API.Controllers;
 [ApiController]
 public class PeopleController : ControllerBase
 {
-    static List<Person> PeopleList = new List<Person>
+    private readonly IPersonService _personService;
+
+    public PeopleController(IPersonService personService)
     {
-        new Person {ID = 1, FirstName = "John", LastName = "Doe", DateOfBirth = new DateTime(1990, 1, 1), Address = "123 Main St", Phone = "3902139210", Gender = 'M', ImagePath = null},
-        new Person {ID = 2, FirstName = "Jane", LastName = "Smith", DateOfBirth = new DateTime(1995, 5, 15), Address = "456 Elm St", Phone = "390213921", Gender = 'F', ImagePath = null},
-        new Person {ID = 3, FirstName = "Alice", LastName = "Johnson", DateOfBirth = new DateTime(1988, 3, 20), Address = "789 Oak St", Phone = "390213", Gender = 'F', ImagePath = null},
-    };
+        _personService = personService;
+    }
+
 
     [HttpGet(Name = "GetAllPeople")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public ActionResult<IEnumerable<Person>> GetAllPeople()
+    public ActionResult<IEnumerable<PersonDTO>> GetAllPeople()
     {
-        List<Person> people = PeopleList;
+        List<PersonDTO> people = _personService.GetAll();
 
         if (people == null || people.Count == 0)
             return NotFound("No people found in the system");
 
         return Ok(people);
 
+    }
+
+    // GET: api/People/5
+    [HttpGet("{id:int}", Name = "GetPersonById")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<PersonDTO> GetPersonById(int id)
+    {
+        var person = _personService.GetById(id);
+
+        if (person == null)
+            return NotFound($"Person with ID {id} not found.");
+
+        return Ok(person);
     }
 
 
