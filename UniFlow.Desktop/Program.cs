7af5f96a5d87193d1f276cf27658fbc1a3a1e00f
Reda Hilal <reda0x04@gmail.com>
2025-07-10 08:43:42 +0400

    init desktop app

---
+++ UniFlow.Desktop/Program.cs
@@ -0,0 +1,20 @@
+namespace UniFlow.Desktop;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+
+        #pragma warning disable WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        Application.SetColorMode(SystemColorMode.Dark);
+        
+        Application.Run(new Form1());
+    }
+}
